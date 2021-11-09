ef881a94a3bc4559ca206974de3e2ebb69b233a0
QKM-0203 <com_qkm@xiyoulinux.org>
2021-11-09 16:09:47 +0800

    style:add space、blank line

--- src/main/Java/chart/Hot212.java
+++ src/main/Java/chart/Hot212.java
@@ -1,4 +1,4 @@
-package Chart;
+package chart;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -8,11 +8,12 @@ import java.util.Set;
 public class Hot212 {
     class Solution {
         Set<String> set = new HashSet<>();
+
         public List<String> findWords(char[][] board, String[] words) {
             for (String word : words) {
                 for (int i = 0; i < board.length; i++) {
                     for (int j = 0; j < board[i].length; j++) {
-                        if(DFS(board,i,j,word,0)){
+                        if (DFS(board, i, j, word, 0)) {
                             set.add(word);
                         }
                     }
@@ -21,36 +22,36 @@ public class Hot212 {
             return new ArrayList<>(set);
         }
 
-        private boolean DFS(char[][] board, int i, int j, String word,int k) {
-            if(i >= board.length || i < 0 || j >= board[i].length || j < 0 || board[i][j]=='0'){
+        private boolean DFS(char[][] board, int i, int j, String word, int k) {
+            if (i >= board.length || i < 0 || j >= board[i].length || j < 0 || board[i][j] == '0') {
                 return false;
             }
-            if(word.charAt(k) != board[i][j]){
-                return false ;
+            if (word.charAt(k) != board[i][j]) {
+                return false;
             }
-            if(k == word.length()-1){
+            if (k == word.length() - 1) {
 
                 return true;
             }
             board[i][j] = '0';
-            boolean right = DFS(board,i,j+1,word,k+1);
-            if(right){
+            boolean right = DFS(board, i, j + 1, word, k + 1);
+            if (right) {
                 board[i][j] = word.charAt(k);
                 return true;
             }
-            boolean up =  DFS(board,i-1,j,word,k+1);
-            if(up){
+            boolean up = DFS(board, i - 1, j, word, k + 1);
+            if (up) {
                 board[i][j] = word.charAt(k);
                 return true;
             }
-            boolean left = DFS(board,i,j-1,word,k+1);
-            if(left){
+            boolean left = DFS(board, i, j - 1, word, k + 1);
+            if (left) {
                 board[i][j] = word.charAt(k);
                 return true;
 
             }
-            boolean down = DFS(board,i+1,j,word,k+1);
-            if(down){
+            boolean down = DFS(board, i + 1, j, word, k + 1);
+            if (down) {
                 board[i][j] = word.charAt(k);
                 return true;
             }
---
+++ src/main/Java/linkedlist/Hot23.java
@@ -0,0 +1,58 @@
+package linkedlist;
+
+/**
+ * 合并个升序链表
+ */
+class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode() {
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Hot23 {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode pc = new ListNode();
+        ListNode p = pc;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                pc.next = l1;
+                l1 = l1.next;
+            } else {
+                pc.next = l2;
+                l2 = l2.next;
+            }
+            pc = pc.next;
+        }
+        if (l1 == null) {
+            pc.next = l2;
+        } else {
+            pc.next = l1;
+        }
+        return p.next;
+    }
+
+    //合并k个升序列表，0和1 2和3 最终还是两两合并，每次改变len就行
+    public ListNode mergeKLists(ListNode[] lists) {
+        int len = lists.length;
+        if (len == 0) return null;
+        while (len > 1) {
+            int k = 0;
+            for (int i = 0; i < len; i += 2) {
+                lists[k++] = i + 1 < len ? mergeTwoLists(lists[i], lists[i + 1]) : lists[i];
+            }
+            len = k;
+        }
+        return lists[0];
+    }
+}
