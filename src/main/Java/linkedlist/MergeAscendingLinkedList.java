81706f64ddc53132fe675815ab702bbad449ff2f
QKM-0203 <com_qkm@xiyoulinux.org>
2021-11-16 23:37:10 +0800

    style:Extract ListNode Class

--- src/main/Java/linkedlist/MergeAscendingLinkedList.java
+++ src/main/Java/linkedlist/MergeAscendingLinkedList.java
@@ -3,23 +3,6 @@ package linkedlist;
 /**
  * Merge ascending linked list
  */
-class ListNode {
-    int val;
-    ListNode next;
-
-    public ListNode() {
-    }
-
-    public ListNode(int val) {
-        this.val = val;
-    }
-
-    public ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
-}
-
 public class MergeAscendingLinkedList {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode pc = new ListNode();
