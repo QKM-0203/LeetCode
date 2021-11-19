1e7017222ab236683374215f84bab67cd1ce9fbe
QKM-0203 <com_qkm@xiyoulinux.org>
2021-11-19 22:21:06 +0800

    notes:add notes

--- src/main/Java/linkedlist/ReverseLinkedList.java
+++ src/main/Java/linkedlist/ReverseLinkedList.java
@@ -1,19 +1,27 @@
 package linkedlist;
 
+//ReverseLinkedList
 public class ReverseLinkedList {
     public ListNode reverseList(ListNode head) {
         if (head == null) {
             return null;
         }
+        //首元素
         ListNode Head = head;
+        //为了将首元素.next=null
         ListNode tempHead = head;
         head = head.next;
         while (head != null) {
+            //留下之后的
             ListNode remainingHead = head.next;
+            //头插
             head.next = Head;
+            //更改头节点
             Head = head;
+            //遍历留下之后的
             head = remainingHead;
         }
+        //原先的首元素.next=null
         tempHead.next = null;
         return tempHead;
     }
