6250427d8d749e846c24e03d7f9ae9ab6c3b1a8f
QKM-0203 <com_qkm@xiyoulinux.org>
2021-11-20 23:02:44 +0800

    add:delete node

---
+++ src/main/Java/linkedlist/DeleteTheNthNode.java
@@ -0,0 +1,21 @@
+package linkedlist;
+//removeNthFromEnd
+public class DeleteTheNthNode {
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode tempHead = new ListNode();
+        tempHead.next = head;
+        ListNode fast = tempHead;
+        ListNode slow = tempHead;
+        while (n > 0) {
+            fast = fast.next;
+            n--;
+        }
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+        return tempHead.next;
+    }
+}
