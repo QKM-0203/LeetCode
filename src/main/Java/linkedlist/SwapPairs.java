c663ca5dd21ec0bd3c94b943597a574cb3ad24ed
QKM-0203 <com_qkm@xiyoulinux.org>
2021-11-27 09:59:37 +0800

    delete:delete method (TwoPoint)

--- src/main/Java/linkedlist/SwapPairs.java
+++ src/main/Java/linkedlist/SwapPairs.java
@@ -1,32 +1,21 @@
 package linkedlist;
+
 //SwapPairs
 public class SwapPairs {
-    //twoPoint
-    public ListNode swapPairsTwoPoint(ListNode head) {
-        ListNode VMHead = new ListNode();
-        VMHead.next = head;
-        ListNode left = VMHead;
-        ListNode right = VMHead.next;
-        while (left && right && right.next){
-            //TODO
-        }
-        return head;
-    }
-    //base method
     public ListNode swapPairs(ListNode head) {
-         if (head == null) {
-             return null;
-         }
-         if (head.next == null) {
-             return head;
-         }
+        if (head == null) {
+            return null;
+        }
+        if (head.next == null) {
+            return head;
+        }
         ListNode headNext = head.next;
         ListNode headNextNext = headNext.next;
         headNext.next = head;
         head.next = headNextNext;
         head = headNext;
         ListNode temp = head.next;
-        while(temp.next != null) {
+        while (temp.next != null) {
             ListNode temp1 = temp.next;
             if (temp1.next == null) {
                 return head;
