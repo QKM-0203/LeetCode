b81ee984870a3b6c473285f44d19c55584b8f82d
QKM-0203 <com_qkm@xiyoulinux.org>
2021-11-27 10:05:02 +0800

    add:LinkedList Intersect

---
+++ src/main/Java/linkedlist/LinkedListIntersect.java
@@ -0,0 +1,43 @@
+package linkedlist;
+
+//LinkedListIntersect
+public class LinkedListIntersect {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode curA = headA;
+        ListNode curB = headB;
+        int lenA = 0, lenB = 0;
+        while (curA != null) { // 求链表A的长度
+            lenA++;
+            curA = curA.next;
+        }
+        while (curB != null) { // 求链表B的长度
+            lenB++;
+            curB = curB.next;
+        }
+        curA = headA;
+        curB = headB;
+        //让curA是最长的指针
+        if (lenB > lenA) {
+            int tempLen = lenA;
+            lenA = lenB;
+            lenB = tempLen;
+            ListNode tempNode = curA;
+            curA = curB;
+            curB = tempNode;
+        }
+        int gap = lenA - lenB;
+        // 让curA和curB在同一起点上（末尾位置对齐）
+        while (gap-- > 0) {
+            curA = curA.next;
+        }
+        // 遍历curA 和 curB，遇到相同则直接返回
+        while (curA != null) {
+            if (curA == curB) {
+                return curA;
+            }
+            curA = curA.next;
+            curB = curB.next;
+        }
+        return null;
+    }
+}
