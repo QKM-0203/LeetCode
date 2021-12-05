eeedb6b148504ffb7971108eb28deb23d1f09367
QKM-0203 <com_qkm@xiyoulinux.org>
2021-12-05 13:04:40 +0800

    add:add queue implement stack

--- src/main/Java/StackAndQueue/ImplementAQueueWithTwoStacks.java
+++ src/main/Java/StackAndQueue/ImplementAQueueWithTwoStacks.java
@@ -7,8 +7,8 @@ import java.util.LinkedList;
  * 思路：两个栈来实现，stackOne是push元素的，将stackOne中的元素出栈放入stackTwo，就可以实现removeHead
  */
 public class ImplementAQueueWithTwoStacks {
-    private Deque<Integer> stackOne;
-    private Deque<Integer> stackTwo;
+    private final Deque<Integer> stackOne;
+    private final Deque<Integer> stackTwo;
 
     public ImplementAQueueWithTwoStacks() {
         stackOne = new LinkedList<>();
@@ -30,5 +30,69 @@ public class ImplementAQueueWithTwoStacks {
             }
         }
         return stackTwo.removeLast();
+    }
+}
+
+class MyQueue {
+    private final Deque<Integer> stackOne;
+    private final Deque<Integer> stackTwo;
+
+
+    public MyQueue() {
+        stackOne = new LinkedList<>();
+        stackTwo = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        stackOne.push(x);
+    }
+
+    public int pop() {
+        return pushStackTwo();
+    }
+
+    public int peek() {
+        return pushStackTwo();
+    }
+
+    public boolean empty() {
+        return stackOne.isEmpty() && stackTwo.isEmpty();
+    }
+
+    public int pushStackTwo() {
+        if (stackTwo.isEmpty()) {
+            if (stackOne.isEmpty()) {
+                return -1;
+            } else {
+                while (!stackOne.isEmpty()) {
+                    stackTwo.push(stackOne.pop());
+                }
+            }
+        }
+        return stackTwo.peek();
+    }
+}
+
+class MyStack {
+    private final Deque<Integer> stackOne;
+
+    public MyStack() {
+        stackOne = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        stackOne.push(x);
+    }
+
+    public int pop() {
+        return stackOne.pop();
+    }
+
+    public int top() {
+        return stackOne.peek();
+    }
+
+    public boolean empty() {
+        return stackOne.isEmpty();
     }
 }
